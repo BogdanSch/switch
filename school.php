bc88af6d76b6ffa85e49f46069d13043de492cea
Bohdan <bogmira777@gmail.com>
2022-09-18 14:06:00 +0200

    Add school task

--- school.php
+++ school.php
@@ -12,8 +12,29 @@ switch ($class) {
     case 3: 
         echo "$class Вчимо задачі";
         break;
+    case 4: 
+        echo "$class Вчимо рівняння";
+        break;
     case 5: 
-        echo "$class Вчимо таблицю множення";
+        echo "$class Вчимо Дроби";
+        break;
+    case 6: 
+        echo "$class Вчимо програмування";
+        break;
+    case 7: 
+        echo "$class Вчимо вірші";
+        break;
+    case 8: 
+        echo "$class Вчимо трикутники";
+        break;
+    case 9: 
+        echo "$class Вчимо тригонометрію";
+        break;
+    case 10: 
+        echo "$class Вчимо стериометрію";
+        break;
+    case 11: 
+        echo "$class Готуємось до ЗНО";
         break;
     case 12:
         echo "$class Майже всі вивчили!";
